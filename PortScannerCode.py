b81099f5befb3d4aa417a24728fa8657a3cb99f2
JADEBIT <72592284+Jadebit-li@users.noreply.github.com>
2026-06-18 14:31:56 +0530

    added threading

--- PortScannerCode.py
+++ PortScannerCode.py
@@ -1,11 +1,35 @@
 import socket
+import threading
+from concurrent.futures import ThreadPoolExecutor
+
+lock = threading.Lock()
+results = []
+
+SERVICE_SIGNATURES = {
+    "SSH": "SSH",
+    "HTTP": "HTTP",
+    "FTP": "FTP",
+    "SMTP": "SMTP",
+    "MySQL": "MySQL",
+    "Apache": "Apache",
+}
+
 def scan_port(host, port_number):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         s.settimeout(1)
         s.connect((host, port_number))
-        response = s.recv(1024)
-        return "open: " + response.decode('utf-8', errors='ignore')
+        try: 
+            banner = s.recv(1024)
+        except socket.timeout:
+            s.send(b"HEAD / HTTP/1.0\r\n\r\n")
+            try:
+                banner = s.recv(1024)
+            except socket.timeout:
+                banner = b"open but no banner"
+        decoded = banner.decode('utf-8', errors='ignore')
+        service = identify_service(decoded)
+        return f" | open | {service} | {decoded}"
     except ConnectionRefusedError:
         return "closed"
     except socket.timeout:
@@ -13,6 +37,26 @@ def scan_port(host, port_number):
     finally:
         s.close()
 
-for i in range (1, 101):
-    c = scan_port("scanme.nmap.org", i)
-    print(c)
+def identify_service(banner):
+    for key, value in SERVICE_SIGNATURES.items():
+        if key in banner:
+            return value
+    return "Unknown"
+
+def scan_and_store(port):
+    result = scan_port("scanme.nmap.org", port)
+    if "open" in result:
+        with lock:
+            results.append(f"Port {port} {result}")
+
+
+# for i in range (1, 101):
+#     c = scan_port("scanme.nmap.org", i)
+#     print(c)
+#instead of this, we use:
+
+with ThreadPoolExecutor(max_workers=50) as executor:
+    executor.map(scan_and_store, range(1,1001))
+
+for r in sorted(results):
+    print(r)
