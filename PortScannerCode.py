e7975d46847bdf9af797857dc8753d13449510a5
JADEBIT <72592284+Jadebit-li@users.noreply.github.com>
2026-06-20 22:59:59 +0530

    added CVE

--- PortScannerCode.py
+++ PortScannerCode.py
@@ -2,6 +2,7 @@ import socket
 import threading
 from concurrent.futures import ThreadPoolExecutor
 import json
+import re
 with open("cve_database.json") as f:
     CVE_DB = json.load(f)
 
@@ -9,12 +10,13 @@ lock = threading.Lock()
 results = []
 
 SERVICE_SIGNATURES = {
-    "SSH": "SSH",
-    "HTTP": "HTTP",
+    "OpenSSH": "OpenSSH",
+    "Apache": "Apache",
+    "nginx": "nginx",
     "FTP": "FTP",
     "SMTP": "SMTP",
     "MySQL": "MySQL",
-    "Apache": "Apache",
+    "HTTP": "HTTP",
 }
 
 def scan_port(host, port_number):
@@ -30,9 +32,12 @@ def scan_port(host, port_number):
                 banner = s.recv(1024)
             except socket.timeout:
                 banner = b"open but no banner"
-        decoded = banner.decode('utf-8', errors='ignore').split('\n')[0].strip()
-        service = identify_service(decoded)
-        return f" | open | {service} | {decoded}"
+        decoded_full = banner.decode('utf-8', errors='ignore')
+        decoded = decoded_full.split('\n')[0].strip()
+        service = identify_service(decoded_full)
+        version = extract_version(decoded_full)
+        CVE = check_cve(service, version)
+        return f" | open | {service} | {decoded} | CVEs: {CVE}"
     except ConnectionRefusedError:
         return "closed"
     except socket.timeout:
@@ -52,6 +57,20 @@ def scan_and_store(port):
         with lock:
             results.append(f"Port {port} {result}")
 
+def extract_version(banner):
+    match = re.search(r"\d+\.\d+\.\d+[a-zA-Z0-9]*", banner)
+    if match:
+        version = match.group()
+    else:
+        version = "unknown"
+    return version
+
+
+def check_cve(service, version): # takes two separate strings
+    #service is like the name of the person and version like phone number, and thats how we saved them in the database
+    key = service + " " + version    # glue them into one string matching dict format
+    return CVE_DB.get(key, [])   # look it up safely, empty list if not found
+
 
 # for i in range (1, 101):
 #     c = scan_port("scanme.nmap.org", i)
@@ -63,10 +82,3 @@ with ThreadPoolExecutor(max_workers=50) as executor:
 
 for r in sorted(results):
     print(r)
-
-def check_cve(service, version): # takes two separate strings
-    #service is like the name of the person and version like phone number, and thats how we saved them in the database
-    key = service + " " + version    # glue them into one string matching dict format
-    return CVE_DB.get(key, [])   # look it up safely, empty list if not found
-
-print(check_cve("OpenSSH", "6.6.1p1"))
